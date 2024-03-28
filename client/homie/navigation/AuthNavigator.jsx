bb832e22fbec469b1b8702289ef6a137607bc0fb
mrhairinson <nguyenbahai12122001@gmail.com>
2024-03-28 09:48:56 +0700

    fix: _RNGestureHandlerModule.default.flushOperations

--- client/homie/navigation/AuthNavigator.jsx
+++ client/homie/navigation/AuthNavigator.jsx
@@ -1,26 +1,21 @@
-import { createStackNavigator } from '@react-navigation/stack'
-import React from 'react'
-import { Signin, Signup, Verification, NotLogin} from '../screens';
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import NotLogin from "../screens/NotLogin";
+import Signin from "../screens/Signin";
+import Signup from "../screens/Signup";
+import Verification from "../screens/Verification";
 
 const Stack = createStackNavigator();
 
 const AuthNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName='NotLogin'>
-      <Stack.Screen 
-        name="NotLogin" 
-        component={NotLogin} />
-      <Stack.Screen 
-        name="Signin" 
-        component={Signin} />
-      <Stack.Screen 
-        name="Signup" 
-        component={Signup} />
-      <Stack.Screen 
-        name="Verification" 
-        component={Verification} />
+    <Stack.Navigator initialRouteName="NotLogin">
+      <Stack.Screen name="NotLogin" component={NotLogin} />
+      <Stack.Screen name="Signin" component={Signin} />
+      <Stack.Screen name="Signup" component={Signup} />
+      <Stack.Screen name="Verification" component={Verification} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
-export default AuthNavigator
+export default AuthNavigator;
--- client/homie/navigation/BottomTabNavigator.jsx
+++ client/homie/navigation/BottomTabNavigator.jsx
@@ -1,71 +1,77 @@
-import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
-import React from 'react'
-import { Search, Map, Message, Profile } from '../screens'
-import { Ionicons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { FontAwesome } from '@expo/vector-icons';
-import COLOR from '../constants/color';
+import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome } from "@expo/vector-icons";
+import COLOR from "../constants/color";
+import Search from "../screens/Search";
+import Map from "../screens/Map";
+import Message from "../screens/Message";
+import Profile from "../screens/Profile";
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator 
-        initialRouteName='Search'
-        screenOptions={({route}) => (
-          {
-            tabBarActiveTintColor: COLOR.PRIMARY,
-            tabBarShowLabel: false,
-            headerShown: false,
-            tabBarStyle: {display: getTabbarVisibility(route)},
-          }
-        )}>
-        <Tab.Screen 
-          name="Search" 
-          component={Search} 
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="search-outline" size={size} color={color} />
-            )
-          }}
-        />
-        <Tab.Screen 
-          name="Map" 
-          component={Map}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="map-outline" size={size} color={color} />
-            )
-          }} 
-        />
-        <Tab.Screen  
-          name="Message" 
-          component={Message} 
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="chatbubble-outline" size={size} color={color} />
-            )
-          }} 
-        />
-        <Tab.Screen 
-          name="Profile" 
-          component={Profile} 
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <FontAwesome name="user-o" size={size} color={color} />
-            )
-          }} 
-        />
-      </Tab.Navigator>
-  )
-}
-
-const getTabbarVisibility = (route) => {
-  const routeName = getFocusedRouteNameFromRoute(route) ?? 'Search';
-  if (routeName === 'Signup' || routeName === 'Verification' || routeName === 'Signin') {
-    return 'none';
-  }
-  return 'visible';
+    <Tab.Navigator
+      initialRouteName="Search"
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: COLOR.PRIMARY,
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarStyle: { display: getTabbarVisibility(route) },
+      })}
+    >
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Map"
+        component={Map}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="map-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Message"
+        component={Message}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="chatbubble-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="user-o" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 };
 
-export default BottomTabNavigator
+const getTabbarVisibility = (route) => {
+  const routeName = getFocusedRouteNameFromRoute(route) ?? "Search";
+  if (
+    routeName === "Signup" ||
+    routeName === "Verification" ||
+    routeName === "Signin"
+  ) {
+    return "none";
+  }
+  return "visible";
+};
+
+export default BottomTabNavigator;
